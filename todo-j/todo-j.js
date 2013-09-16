0038314cb93bc2bf4ffa75bdc413962d743caf1b
Varya Stepanova <mail@varya.me>
2013-09-16 15:27:47 +0200

    Comments on View

--- todo-j/todo-j.js
+++ todo-j/todo-j.js
@@ -60,13 +60,19 @@ var TodoList = Backbone.Collection.extend({
 // Create our global collection of **Todos**.
 var Todos = new TodoList;
 
-// View
+// Todo Item View
+// --------------
+
+// The DOM element for a todo item...
 var TodoView = Backbone.View.extend({
 
+    //... is a list tag.
     tagName:  "li",
 
+    // Cache the template function for a single item.
     template: _.template($('#item-template').html()),
 
+    // The DOM events specific to an item.
     events: {
         "click .toggle"   : "toggleDone",
         "dblclick .view"  : "edit",
@@ -75,11 +81,15 @@ var TodoView = Backbone.View.extend({
         "blur .edit"      : "close"
     },
 
+    // The TodoView listens for changes to its model, re-rendering. Since there's
+    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
+    // app, we set a direct reference on the model for convenience.
     initialize: function() {
         this.listenTo(this.model, 'change', this.render);
         this.listenTo(this.model, 'destroy', this.remove);
     },
 
+    // Re-render the titles of the todo item.
     render: function() {
         this.$el.html(this.template(this.model.toJSON()));
         this.$el.toggleClass('done', this.model.get('done'));
@@ -87,15 +97,18 @@ var TodoView = Backbone.View.extend({
         return this;
     },
 
+    // Toggle the `"done"` state of the model.
     toggleDone: function() {
         this.model.toggle();
     },
 
+    // Switch this view into `"editing"` mode, displaying the input field.
     edit: function() {
         this.$el.addClass("editing");
         this.input.focus();
     },
 
+    // Close the `"editing"` mode, saving changes to the todo.
     close: function() {
         var value = this.input.val();
         if (!value) {
@@ -106,10 +119,12 @@ var TodoView = Backbone.View.extend({
         }
     },
 
+    // If you hit `enter`, we're through editing the item.
     updateOnEnter: function(e) {
         if (e.keyCode == 13) this.close();
     },
 
+    // Remove the item, destroy the model.
     clear: function() {
         this.model.destroy();
     }
