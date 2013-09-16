a73bff0ac7a89a64a2447d439475531cc447b2b2
Varya Stepanova <mail@varya.me>
2013-09-16 15:30:59 +0200

    Comments on application

--- todo-j/todo-j.js
+++ todo-j/todo-j.js
@@ -131,19 +131,29 @@ var TodoView = Backbone.View.extend({
 
 });
 
-    // Application
+// The Application
+// ---------------
+
+// Our overall **AppView** is the top-level piece of UI.
 var AppView = Backbone.View.extend({
 
+    // Instead of generating a new element, bind to the existing skeleton of
+    // the App already present in the HTML.
     el: $("#todoapp"),
 
+    // Our template for the line of statistics at the bottom of the app.
     statsTemplate: _.template($('#stats-template').html()),
 
+    // Delegated events for creating new items, and clearing completed ones.
     events: {
         "keypress #new-todo":  "createOnEnter",
         "click #clear-completed": "clearCompleted",
         "click #toggle-all": "toggleAllComplete"
     },
 
+    // At initialization we bind to the relevant events on the `Todos`
+    // collection, when items are added or changed. Kick things off by
+    // loading any preexisting todos that might be saved in *localStorage*.
     initialize: function() {
 
         this.input = this.$("#new-todo");
@@ -160,6 +170,8 @@ var AppView = Backbone.View.extend({
 
     },
 
+    // Re-rendering the App just means refreshing the statistics -- the rest
+    // of the app doesn't change.
     render: function() {
         var done = Todos.done().length;
         var remaining = Todos.remaining().length;
@@ -176,15 +188,20 @@ var AppView = Backbone.View.extend({
         this.allCheckbox.checked = !remaining;
     },
 
+    // Add a single todo item to the list by creating a view for it, and
+    // appending its element to the `<ul>`.
     addOne: function(todo) {
         var view = new TodoView({model: todo});
         this.$("#todo-list").append(view.render().el);
     },
 
+    // Add all items in the **Todos** collection at once.
     addAll: function() {
         Todos.each(this.addOne, this);
     },
 
+    // If you hit return in the main input field, create new **Todo** model,
+    // persisting it to *localStorage*.
     createOnEnter: function(e) {
         if (e.keyCode != 13) return;
         if (!this.input.val()) return;
@@ -193,6 +210,7 @@ var AppView = Backbone.View.extend({
         this.input.val('');
     },
 
+    // Clear all done todo items, destroying their models.
     clearCompleted: function() {
         _.invoke(Todos.done(), 'destroy');
         return false;
@@ -205,6 +223,7 @@ var AppView = Backbone.View.extend({
 
 });
 
+// Finally, we kick things off by creating the **App**.
 var App = new AppView;
 
 });
