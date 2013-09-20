ed31c9a19ede5003b09b1b3f7c04d01ca3198d71
Varya Stepanova <mail@varya.me>
2013-09-20 13:13:48 +0200

    Changing a property and listening for the corresponding event

--- joe-zim/joe-zim.js
+++ joe-zim/joe-zim.js
@@ -1,6 +1,9 @@
 var Person = Backbone.Model.extend({
 
     initialize: function() {
+        this.bind('change:height', function(){
+            console.log(this.get('name'), ' height changed');
+        });
         console.log('Hello world!');
     },
     defaults: {
@@ -15,5 +18,7 @@ var varya = new Person({ name: 'Varya', height: '163 cm' });
 console.log(varya.get('name'));
 
 var someone = new Person();
+someone.set('height', '180 cm');
 
 console.log(someone.get('name'));
+console.log(someone.get('height'));
