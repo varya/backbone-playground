859ffebf82e45a0666598400a056c198ff93bb81
Varya Stepanova <mail@varya.me>
2013-09-20 13:24:08 +0200

    Catching an error when invalid

--- joe-zim/joe-zim.js
+++ joe-zim/joe-zim.js
@@ -4,21 +4,32 @@ var Person = Backbone.Model.extend({
         this.bind('change:height', function(){
             console.log(this.get('name'), ' height changed');
         });
+        this.on('invalid', function(m, error){
+            console.log(error);
+        });
         console.log('Hello world!');
     },
     defaults: {
         name: 'Alex'
+    },
+    validate: function(attrs){
+        if (attrs.height < 150) {
+            return 'You are not very tall for this job';
+        }
     }
 
 });
 
 
-var varya = new Person({ name: 'Varya', height: '163 cm' });
+var varya = new Person({ name: 'Varya', height: 163 });
 
 console.log(varya.get('name'));
 
 var someone = new Person();
-someone.set('height', '180 cm');
+someone.set('height', 180, { validate: true });
 
 console.log(someone.get('name'));
 console.log(someone.get('height'));
+
+var kid = new Person({name: 'Tanya'});
+kid.set('height', 130, { validate: true });
