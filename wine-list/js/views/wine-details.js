0e36477e8bbcb99b6feeec8ca2ff7befa8ab403f
Varya Stepanova <mail@varya.me>
2013-09-23 12:45:10 +0200

    A View for Details of wine

---
+++ wine-list/js/views/wine-details.js
@@ -0,0 +1,58 @@
+window.WineView = Backbone.View.extend({
+
+    initialize: function() {
+        this.template = _.template($('#wine-details-template').html());
+
+        this.model.bind('change', this.render, this);
+    },
+
+    render: function() {
+        this.$el.html( this.template(this.model.toJSON()));
+
+        return this.el;
+    },
+
+    events: {
+
+        'click .save': 'saveWimne',
+        'click .delete': 'deleteWine'
+
+    },
+
+    saveWine: function() {
+        this.model.set({
+            name: $('#name').val(),
+            grapes: $('#grapes').val(),
+            country: $('#country').val(),
+            region: $('#region').val(),
+            year: $('#year').val(),
+            description: $('#description').val()
+        });
+
+        if(this.model.isNew()) {
+            var self = this;
+
+            app.wineList.create(this.model, {
+
+                'success' : function() {
+                    app.navigate( 'wines/' + self.model.id, false);
+                }
+
+            });
+        } else {
+            this.model.save();
+        }
+    },
+
+    deleteWine: function() {
+      this.model.destroy({
+            success: function() {
+                alert('Wine was deleted successfully');
+                window.history.back();
+            }
+        });
+
+        return false;
+    }
+
+});
